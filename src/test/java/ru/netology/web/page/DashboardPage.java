9cb9d66b857d42f30f38098df956b4f934a4c5e4
Алена Кудрявцева <sunny_aga@mail.ru>
2022-11-08 14:21:59 +0300

    test

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -16,15 +16,17 @@ public class DashboardPage {
         heading.shouldBe(visible);
     }
 
-    private int extractBalance(String text){
+    private int extractBalance(String text) {
         val start = text.indexOf(balanceStart);
         val finish = text.indexOf(balanceFinish);
         val value = text.substring(start + balanceStart.length(), finish);
         return Integer.parseInt(value);
     }
+
     private ElementsCollection cards = $$(".list__item div");
     private final String balanceStart = "баланс: ";
     private final String balanceFinish = " р.";
+
     public int getCardBalance(int index) {
         var card = cards.get(index);
         val text = card.text();
--- src/test/java/ru/netology/web/page/VerificationPage.java
+++ src/test/java/ru/netology/web/page/VerificationPage.java
@@ -10,13 +10,13 @@ public class VerificationPage {
     private SelenideElement codeField = $("[data-test-id=code] input");
     private SelenideElement verifyButton = $("[data-test-id=action-verify]");
 
-    public VerificationPage(){
+    public VerificationPage() {
         codeField.shouldBe(visible);
-}
+    }
 
-public DashboardPage validVerify(DataHelper.VerificationCode verificationCode) {
-    codeField.setValue(verificationCode.getCode());
-    verifyButton.click();
-    return new DashboardPage();
+    public DashboardPage validVerify(DataHelper.VerificationCode verificationCode) {
+        codeField.setValue(verificationCode.getCode());
+        verifyButton.click();
+        return new DashboardPage();
     }
 }
